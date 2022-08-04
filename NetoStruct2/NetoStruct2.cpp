53de7e75d90433bd7462565ea48ab3898298fc63
DenisMir <pete4kob12@mail.ru>
2022-08-04 22:03:09 +0300

    work2

---
+++ NetoStruct2/NetoStruct2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+
+struct bankUser
+{
+    int numberUser;
+    std::string NameU;
+    double balance;
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    bankUser UserDate;
+    std::cout << "Введите номер счёта: \n";
+    std::cin >> UserDate.numberUser;
+
+    std::cout << "Введите имя владельца: \n";
+    std::cin >> UserDate.NameU;
+
+    std::cout << "Введите баланс: \n";
+    std::cin >> UserDate.balance;
+
+    double newBalance = 0;
+    std::cout << "Введите новый баланс: \n";
+    std::cin >> newBalance;
+
+    UserDate.balance = newBalance;
+
+    std::cout << "Ваш счёт:" << UserDate.NameU << ", " << UserDate.numberUser << ", " << UserDate.balance << ", ";
+}
